4e0118d1acec8c64b125103e6f20221740fb097c
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-12-18 09:41:28 -0600

    created baseline methods for create/update/destroy. Spec test created and passing for creating a poster.

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -4,5 +4,28 @@ class Api::V1::PostersController < ApplicationController
         render json: Poster.all
     end
 
+    def show
+        poster = Poster.find(params[:id])
+        render json: poster
+    end
 
+    
+    def create
+        poster = Poster.create(poster_params)
+        render json: poster
+    end
+
+    def update
+        render json: Postere.update(params[:id], poster_params)    
+    end
+
+    def destroy
+        render json: Poster.delete(params[:id])
+    end
+
+    private
+
+    def poster_params
+        params.require(:poster).permit(:name, :description, :price, :year, :vintage, :img_url)
+    end
 end

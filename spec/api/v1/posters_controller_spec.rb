b5782bc3a667a5811c58111a2f2ff816a43b9f56
Kristin Weiland <kristinweiland@gmail.com>
2024-12-18 18:48:19 -0800

    Cleanup repeated code and add a test for the delete a poster action.

--- spec/api/v1/posters_controller_spec.rb
+++ spec/api/v1/posters_controller_spec.rb
@@ -1,39 +1,9 @@
 require 'rails_helper'
 
-describe "Posters API", type: :request do
-  it 'fetches posters' do
-Poster.destroy_all
+describe "Posters API", type: :request do    
 
-regret = Poster.create!(
-    name: "REGRET",
-    description: "Hard work rarely pays off.",
-    price: 89.00,
-    year: 2018,
-    vintage: true,
-    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
-)
-
-failure = Poster.create!(
-    name: "FAILURE",
-    description: "Why bother trying? It's probably not worth it.",
-    price: 68.00,
-    year: 2019,
-    vintage: true,
-    img_url: "https://images.unsplash.com/photo-1620401537439-98e94c004b0d"
-)
-
-mediocrity = Poster.create!(
-    name: "MEDIOCRITY",
-    description: "Dreams are just that—dreams.",
-    price: 127.00,
-    year: 2021,
-    vintage: false,
-    img_url: "https://images.unsplash.com/photo-1551993005-75c4131b6bd8"
-)
-    
   it 'fetches all posters' do
     Poster.destroy_all
-
     @regret = Poster.create!(name: "REGRET",
       description: "Hard work rarely pays off.",
       price: 89.00,
@@ -58,44 +28,30 @@ mediocrity = Poster.create!(
       img_url: "https://images.unsplash.com/photo-1551993005-75c4131b6bd8"
     )
 
-
     get '/api/v1/posters'
 
     expect(response).to be_successful
     posters = JSON.parse(response.body, symbolize_names: true)
     expect(posters.count).to eq(3)
 
-    posters.each do |poster|
-    expect(poster).to have_key(:id)
-    expect(poster[:id]).to be_an(Integer)
-    expect(poster).to have_key(:id)
-    expect(poster[:id]).to be_an(Integer)
+    posters.each do |poster|  
+      expect(poster).to have_key(:id)
+      expect(poster[:id]).to be_an(Integer)
 
-     expect(poster).to have_key(:name)
-     expect(poster[:name]).to be_a(String)
-        
-     expect(poster).to have_key(:description)
-     expect(poster[:description]).to be_a(String)
-        
-     expect(poster).to have_key(:price)
-     expect(poster[:price]).to be_a(Float)
-        
-     expect(poster).to have_key(:year)
-     expect(poster[:year]).to be_a(Integer)
+      expect(poster).to have_key(:name)
+      expect(poster[:name]).to be_a(String)
+          
+      expect(poster).to have_key(:description)
+      expect(poster[:description]).to be_a(String)
+          
+      expect(poster).to have_key(:price)
+      expect(poster[:price]).to be_a(Float)
+          
+      expect(poster).to have_key(:year)
+      expect(poster[:year]).to be_a(Integer)
 
-     expect(poster).to have_key(:img_url)
-     expect(poster[:img_url]).to be_a(String)
-
-    expect(poster).to have_key(:name)
-    expect(poster[:name]).to be_a(String)
-    expect(poster).to have_key(:description)
-    expect(poster[:description]).to be_a(String)
-    expect(poster).to have_key(:price)
-    expect(poster[:price]).to be_a(Float)
-    expect(poster).to have_key(:year)
-    expect(poster[:year]).to be_a(Integer)
-    expect(poster).to have_key(:img_url)
-    expect(poster[:img_url]).to be_a(String)
+      expect(poster).to have_key(:img_url)
+      expect(poster[:img_url]).to be_a(String)
     end
   end
 
@@ -134,7 +90,7 @@ mediocrity = Poster.create!(
     expect(poster[:img_url]).to be_a(String)
   end
 
-  it "can update a poster" do
+  it" can update a poster" do
     regret_poster = Poster.create!(
     name: "REGRET",
     description: "Hard work rarely pays off.",
@@ -149,7 +105,7 @@ mediocrity = Poster.create!(
     headers = {"CONTENT_TYPE" => "application/json"}
 
     patch "/api/v1/posters/#{id}", headers: headers, params: JSON.generate({poster: poster_params})
-
+  
     poster = Poster.find_by(id: id)
 
     expect(response).to be_successful
@@ -158,7 +114,6 @@ mediocrity = Poster.create!(
   end
 
   it 'can create a poster' do
-
     new_poster = {
       "name": "DEFEAT",
       "description": "It's too late to start now.",
@@ -178,6 +133,31 @@ mediocrity = Poster.create!(
     expect(poster[:year]).to eq(new_poster[:year])
     expect(poster[:vintage]).to eq(new_poster[:vintage])
     expect(poster[:img_url]).to eq(new_poster[:img_url])
+  end
+
+  it 'can delete a poster' do
+    #setup
+    id = Poster.create!(name: "REGRET",
+    description: "Hard work rarely pays off.",
+    price: 89.00,
+    year: 2018,
+    vintage: true,
+    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d").id
+
+    #make a GET request to fetch the poster by ID
+    delete "/api/v1/posters/#{id}"
+  
+    #parse the JSON response
+    poster = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+  
+    expect(poster).to be_falsey
+    expect(poster[:name]).to be_nil
+    expect(poster[:description]).to be_nil
+    expect(poster[:price]).to be_nil
+    expect(poster[:year]).to be_nil
+    expect(poster[:img_url]).to be_nil
   end
 
 end
